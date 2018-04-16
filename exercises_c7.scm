6afaf8181d6601841b94f5f1a0ce8b5e844beca8
Fransisco Fernandez Castano <francisco.castano@datastax.com>
2018-04-16 22:16:45 +0200

    Adding more solutions.

--- exercises_c7.scm
+++ exercises_c7.scm
@@ -20,3 +20,52 @@
     (cond
      ((null? s) '())
      (else (cons (car s) (makesetrember (multirember (car s) (cdr s))))))))
+
+(define subset?
+  (lambda (s1 s2)
+    (cond
+     ((null? s1) #t)
+     ((member? (car s1) s2) (subset? (cdr s1) s2))
+     (else #f))))
+
+
+(define eqset?
+  (lambda (s1 s2)
+    (and (subset? s1 s2)
+         (subset? s2 s1))))
+
+(define eqset?
+  (lambda (s1 s2)
+    (cond
+     ((and (null? s1) (null? s2)) #t)
+     ((member? (car s1) s2) (subset? (cdr s1) s2))
+     (else #f))))
+
+(define subset-and?
+  (lambda (s1 s2)
+    (cond
+     ((null? s1) #t)
+     (else (and (member? (car s1) s2)
+                (subset? (cdr s1) s2))))))
+
+(define intersect?
+  (lambda (s1 s2)
+    (cond
+     ((null? s1) #f)
+     (else (or (member? (car s1) s2)
+               (intersect? (cdr s1) s2))))))
+
+(define intersect
+  (lambda (s1 s2)
+    (cond
+     ((null? s1) '())
+     ((member? (car s1) s2) (cons (car s1)
+                                  (intersect (cdr s1) s2)))
+     (else (intersect (cdr s1) s2)))))
+
+(define union
+  (lambda (s1 s2)
+    (cond
+     ((null? s1) s2)
+     ((member? (car s1) s2) (union (cdr s1) s2))
+     (else (cons (car s1) (union (cdr s1) s2))))))
